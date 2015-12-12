1c720565607470746b561585f396ba8cb16d9296
Brian Knox <bknox@digitalocean.com>
2015-12-12 13:45:20 -0500

    Problem: travis-ci images do not have syslog - removing a test

--- logger_test.go
+++ logger_test.go
@@ -2,13 +2,6 @@ package captainslog
 
 import "testing"
 
-func TestNewMostlyFeaturelessLogger(t *testing.T) {
-	_, err := NewMostlyFeaturelessLogger(Local7)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
 func TestCreateLogMessage(t *testing.T) {
 	msg, err := createLogMessage(Fields{"hello": "world"})
 
