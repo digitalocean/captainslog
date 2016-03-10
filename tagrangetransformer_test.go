b197f87880879b976fae2302f3f7ca496fc461b8
Brian Knox <taotetek@gmail.com>
2016-03-10 09:04:47 -0500

    Problem: reaper in tagrangetransformer was not tested

--- tagrangetransformer_test.go
+++ tagrangetransformer_test.go
@@ -1,13 +1,49 @@
 package captainslog
 
-import "testing"
-
-func TestTagRangeTransformerIsTransformer(t *testing.T) {
-}
+import (
+	"testing"
+	"time"
+)
 
 type testCase struct {
 	input  []byte
 	result bool
+}
+
+func TestRangeTransformerTTL(t *testing.T) {
+	input := []byte("<4>2016-03-08T14:59:36.293816+00:00 host.example.com kernel: [15803005.789011] ------------[ cut here ]------------\n")
+
+	transformer := NewTagRangeTransformer(
+		NewTagMatcher("kernel:"),
+		NewContentContainsMatcher("[ cut here ]"),
+		NewContentContainsMatcher("[ end trace"),
+		NewTagArrayMutator("tags", "trace"),
+		1, 1)
+
+	original := NewSyslogMsg()
+	err := Unmarshal(input, &original)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if want, got := 0, len(transformer.trackingDB); want != got {
+		t.Errorf("want '%v', got '%v'", want, got)
+	}
+
+	_, err = transformer.Transform(original)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if want, got := 1, len(transformer.trackingDB); want != got {
+		t.Errorf("want '%v', got '%v'", want, got)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	if want, got := 0, len(transformer.trackingDB); want != got {
+		t.Errorf("want '%v', got '%v'", want, got)
+	}
 }
 
 func TestTagRangeTransformerTransform(t *testing.T) {
@@ -38,7 +74,7 @@ func TestTagRangeTransformerTransform(t *testing.T) {
 		},
 	}
 
-	mutator := NewTagRangeTransformer(
+	transformer := NewTagRangeTransformer(
 		NewTagMatcher("kernel:"),
 		NewContentContainsMatcher("[ cut here ]"),
 		NewContentContainsMatcher("[ end trace"),
@@ -52,7 +88,7 @@ func TestTagRangeTransformerTransform(t *testing.T) {
 			t.Error(err)
 		}
 
-		mutated, err := mutator.Transform(original)
+		mutated, err := transformer.Transform(original)
 		if err != nil {
 			t.Error(err)
 		}
@@ -68,7 +104,7 @@ func TestTagRangeTransformerTransform(t *testing.T) {
 func BenchmarkTagRangeTransformerTransform(b *testing.B) {
 	m := []byte("<4>2016-03-08T14:59:36.293816+00:00 host.example.com kernel: [15803005.789011] ------------[ cut here ]------------\n")
 
-	mutator := NewTagRangeTransformer(
+	transformer := NewTagRangeTransformer(
 		NewTagMatcher("kernel:"),
 		NewContentContainsMatcher("[ cut here ]"),
 		NewContentContainsMatcher("[ end trace"),
@@ -82,7 +118,7 @@ func BenchmarkTagRangeTransformerTransform(b *testing.B) {
 			panic(err)
 		}
 
-		_, err = mutator.Transform(original)
+		_, err = transformer.Transform(original)
 		if err != nil {
 			panic(err)
 		}
