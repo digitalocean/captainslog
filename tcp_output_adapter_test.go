f944a37aa5551fd873de0ed9c6a5dcbf319b87c1
Brian Knox <taotetek@gmail.com>
2016-03-12 11:42:32 -0500

    Problem: TcpOutputAdapter should have test

---
+++ tcp_output_adapter_test.go
@@ -0,0 +1,69 @@
+package captainslog
+
+import (
+	"bufio"
+	"bytes"
+	"net"
+	"sync"
+	"testing"
+)
+
+func TestTCPOutputAdapter(t *testing.T) {
+	testMsg := []byte("<191>2006-01-02T15:04:05.999999-07:00 host.example.org test: hello world\n")
+	var msg SyslogMsg
+	err := Unmarshal(testMsg, &msg)
+	if err != nil {
+		t.Error(err)
+	}
+
+	address := "127.0.0.1:3333"
+	retryInterval := 1
+
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		t.Error(err)
+	}
+	defer l.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+		}
+		defer conn.Close()
+
+		reader := bufio.NewReader(conn)
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			t.Error(err)
+		}
+
+		line = append(line, '\n')
+		if want, got := 0, bytes.Compare(testMsg, line); want != got {
+			t.Errorf("want '%v', got '%v'", want, got)
+			t.Errorf("%s", line)
+		}
+
+		wg.Done()
+	}()
+
+	a := NewTCPOutputAdapter(address, retryInterval)
+	defer a.Close()
+
+	err = a.Connect()
+	if err != nil {
+		t.Error(err)
+	}
+
+	n, err := a.Output(&msg)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if want, got := len(testMsg), n; want != got {
+		t.Errorf("want '%v', got '%v'", want, got)
+	}
+}
